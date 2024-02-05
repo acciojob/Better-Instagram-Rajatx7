c1b4715ad75b3248b10dff1a4757dcfd846236c4
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-02-05 23:02:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+
+  function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+  }
+
+  function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    var draggedElement = document.getElementById(data);
+    var targetElement = event.currentTarget; // Use currentTarget instead of target
+
+    // Swap the background images
+    var tempImage = draggedElement.style.backgroundImage;
+    draggedElement.style.backgroundImage = targetElement.style.backgroundImage;
+    targetElement.style.backgroundImage = tempImage;
+  }
+
